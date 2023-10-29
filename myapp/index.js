8147d857c118f9aeab38108861e6a167df5d797b
gowthampeddi09 <gowthaampeddi09@gmail.com>
2023-10-29 19:02:08 +0000

    Goodreads Get Books API

---
+++ myapp/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const app = express();
+
+const dbPath = path.join(__dirname, "goodreads.db");
+let bd = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server is running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBookQuery = `SELECT * FROM book ORDER BY book_id`;
+  const bookArray = await db.all(getBookQuery);
+  response.send(bookArray);
+});
